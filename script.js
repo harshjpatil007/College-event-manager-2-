8114350213e1c8e900c9142f74d66a678710bc41
Harsh  Patil <harshjpatil007@gmail.com>
2026-04-05 19:56:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,72 @@ function showLogin(){
 document.getElementById("registerBox").classList.add("hidden")
 
 }
+function createEvent(){
+
+let name = document.getElementById("eventName").value
+let date = document.getElementById("eventDate").value
+let venue = document.getElementById("eventVenue").value
+let organizer = document.getElementById("eventOrganizer").value
+
+let event = {
+name:name,
+date:date,
+venue:venue,
+organizer:organizer
+}
+
+let events = JSON.parse(localStorage.getItem("events")) || []
+
+events.push(event)
+
+localStorage.setItem("events",JSON.stringify(events))
+
+alert("Event Created Successfully")
+
+window.location.href="dashboard.html"
+
+}
+
+
+
+function loadEvents(){
+
+let events = JSON.parse(localStorage.getItem("events")) || []
+
+let eventList = document.getElementById("eventList")
+
+if(!eventList) return
+
+eventList.innerHTML=""
+
+events.forEach(event => {
+
+let image = "images/default.jpg"
+
+if(event.organizer.toLowerCase().includes("debuggers")){
+image = "images/debuggers.jpg"
+}
+
+if(event.organizer.toLowerCase().includes("telekinesis")){
+image = "images/telekinesis.jpg"
+}
+
+let card = document.createElement("div")
+
+card.className="event-card"
+
+card.innerHTML = `
+<img src="${image}">
+<h3>${event.name}</h3>
+<p>Date : ${event.date}</p>
+<p>Venue : ${event.venue}</p>
+<p>Organizer : ${event.organizer}</p>
+`
+
+eventList.appendChild(card)
+
+})
+
+}
+
+window.onload = loadEvents
